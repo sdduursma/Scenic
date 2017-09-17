5f088bf42766fe09d502b67baf0fff8c76e36e2b
Samuel Duursma <sd.duursma@gmail.com>
2017-09-17 16:31:46 +0100

    Create Scenic Xcode project

---
+++ Scenic/Scenic.h
@@ -0,0 +1,19 @@
+//
+//  Scenic.h
+//  Scenic
+//
+//  Created by Samuel Duursma on 17/09/2017.
+//  Copyright © 2017 Samuel Duursma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Scenic.
+FOUNDATION_EXPORT double ScenicVersionNumber;
+
+//! Project version string for Scenic.
+FOUNDATION_EXPORT const unsigned char ScenicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scenic/PublicHeader.h>
+
+
